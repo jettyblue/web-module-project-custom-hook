f5e0d65b99bcc993d1bd5f6ef8ae71bfe283dde7
unknown <lindsay.kelly15@gmail.com>
2022-02-09 17:51:57 -0500

    useDarkMode complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import useLocalStorage from './useLocalStorage';
 
 // const [darkMode, setDarkMode] = useState(false);
 {/* <Navbar darkMode={darkMode} setDarkMode={setDarkMode} /> */}
 
-const useDarkMode
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue);
+    return [darkMode, setDarkMode];
+}
 
 export default useDarkMode;
