044ee11bf16ccb237bf278bcd730f1672d6f6819
unknown <lindsay.kelly15@gmail.com>
2022-02-09 18:34:55 -0500

    useLocalStorage complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
 
-// const [darkMode, setDarkMode] = useState(false);
-{/* <Navbar darkMode={darkMode} setDarkMode={setDarkMode} /> */}
-
 const useDarkMode = (initialValue) => {
     const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue);
     return [darkMode, setDarkMode];
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 
-// export default function useLocalStorage() {
-//     const [localStorage, setLocalStorage] = useState()
+const useLocalStorage = (key, initialValue) => {
+    const setIt = value => window.localStorage.setItem(key, JSON.stringify(value))
+    const [state, setState] = useState(() => {
+        const item = window.localStorage.getItem(key)
+        if(item) {
+            return JSON.parse(item);
+        }
+        setIt(initialValue)
+        return initialValue;
+    })
 
+    const setValueLocalStorage = newValue => {
+        setIt(newValue)
+        setValue(newValue)
+    }
+
+    return [value, setValueLocalStorage];
 }
+
+export default useLocalStorage;
